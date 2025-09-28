7d2b8e8b3ee5ae6af82aec3b0d5d85d6245b8581
Geo <88101173+geoo0@users.noreply.github.com>
2025-09-28 21:47:47 +0000

    Hotfix: poner credenciales en config.js para Render

--- src/config.js
+++ src/config.js
@@ -1,16 +1,13 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 export const config = {
   port: process.env.PORT || 3000,
   pg: {
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    port: Number(process.env.PGPORT || 5432),
+    host: "dpg-d3cpm5ali9vc73dq00kg-a.oregon-postgres.render.com",
+    database: "restaurante_ordenes_db_eqoj",
+    user: "restaurante_ordenes_db_eqoj_user",
+    password: "fyHYXNz2YFdNlHZ1S6OWWk3AOG9N1dLI",
+    port: 5432,
     ssl: { rejectUnauthorized: false }
   },
-  schema: process.env.DB_SCHEMA || 'public',
-  corsOrigins: (process.env.CORS_ORIGINS || '').split(',').map(s => s.trim()).filter(Boolean)
+  schema: 'public',
+  corsOrigins: []
 };
